06dc01efff674c2852a431e71c41505511dec17b
AriellaYamada <ariella.y.b@hotmail.com>
2018-10-25 10:47:53 -0300

    adding articles to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,29 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}"
 			redirect_to articles_path
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "Your account updated successfully"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
 	end
 
 	private
