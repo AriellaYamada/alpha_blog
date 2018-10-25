85831f27cd2460822b57efcb630f0108f37f92da
AriellaYamada <ariella.y.b@hotmail.com>
2018-10-25 10:03:30 -0300

    creating user

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 	root 'pages#home'
 	get'about', to: 'pages#about'
 
+	get 'signup', to: 'users#new'
+	post 'users', to: 'users#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
